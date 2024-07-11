65e10e6a09541a8bd35bf1bcc2a70d10312e76b2
ValerieVelazquez <vg.velazquez@ebc.edu.mx>
2024-07-10 18:35:27 -0600

    add initial project config

---
+++ camel/src/main/groovy/com/devs/camel/CamelApplication.groovy
@@ -0,0 +1,13 @@
+package com.devs.camel
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class CamelApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(CamelApplication, args)
+	}
+
+}
