d3684e88f52fb94d480802c258fe788aa65bd6fe
ValerieVelazquez <vg.velazquez@ebc.edu.mx>
2024-07-15 22:00:50 -0600

    delete api description

--- camel/src/main/groovy/com/devs/camel/configurations/SwaggerConfig.groovy
+++ camel/src/main/groovy/com/devs/camel/configurations/SwaggerConfig.groovy
@@ -9,27 +9,13 @@ import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 
-import java.util.Collections;
-
 @Configuration
 public class SpringFoxConfig {
-
-  private ApiInfo apiInfo() {
-    return new ApiInfo(
-      "My REST API",
-      "Some custom description of API.",
-      "API TOS",
-      "Terms of service",
-      new Contact("John Doe", "www.example.com", "myeaddress@company.com"),
-      "License of API",
-      "API license URL",
-      Collections.emptyList());
-  }
 
   @Bean
   public Docket api() {
     return new Docket(DocumentationType.SWAGGER_2)
-    .apiInfo(apiInfo())
+    .apiInfo()
     .select()
     .apis(RequestHandlerSelectors.basePackage("com.devs.camel.controller"))
     .paths(PathSelectors.any())
